a2181ceddd41a57a2c7d989305febd35ba354d37
Ramiro Gonzalez <ramirogc21@gmail.com>
2020-05-30 22:18:07 -0300

    index clean

--- NYCDataAgregator/loader/index.js
+++ NYCDataAgregator/loader/index.js
@@ -5,20 +5,17 @@ const path = require("path");
 
 this.queue = new Queue('data-csv');
 
-let lineCount = 0;
-let csvPath = path.join(__dirname, "../data1.csv");
+let lineCount = 1;
+let csvPath = path.join(__dirname, "./data-parking-issues.csv");
 
 (async (csvPath) => {
 return  await fs.createReadStream(csvPath)
         .pipe(es.split())
         .pipe(es.mapSync((line) => {
-            lineCount += 1;
-            if(lineCount!=1){
-                const issue = { issue: line.toString() }
-                console.log(`Line Count: ${lineCount}`, issue)
-                // this.queue.add(`Line Count: ${lineCount}`,issue);                
-                this.queue.add(issue);
-            }
+            const issue = { issue: line.toString() }
+            console.log(`Line Count: ${lineCount}`, issue)
+            this.queue.add(issue);
+            lineCount++;
         }).on('error', (err) => {
             console.log('Error while reading file.', err);
         }).on('end', () => {
