4ad9c06ed54d78c7debf241c3f9820913c2d0f98
Ramiro Gonzalez <ramirogc21@gmail.com>
2020-06-01 09:17:53 -0300

    initializing all services

--- GoDataQueryTool/index.js
+++ GoDataQueryTool/index.js
@@ -1,7 +1,15 @@
 const Server = require('./server/server');
 const QueryRepository = require('./repositories/queryRepository');
+const GoDataSync = require('./sync/sync')
+const Repository = require('../NYCDataAgregator/sync/repository')
 
 (async () => {
-    await QueryRepository.initRepository();
-    await Server.initServer();
+    try {
+        await Repository.initRepository();
+        await QueryRepository.initRepository();
+        await Server.initServer();
+        await GoDataSync.start();
+    } catch (err) {
+        console.log(err)
+    }
 })();
