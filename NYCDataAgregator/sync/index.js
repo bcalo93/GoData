49cfb7582848780ca29da6ea650e86b641b20d5e
Ramiro Gonzalez <ramirogc21@gmail.com>
2020-06-01 09:26:56 -0300

    NYCDataAggregator index changed to call goDataSync

--- NYCDataAgregator/sync/index.js
+++ NYCDataAgregator/sync/index.js
@@ -1,11 +1,13 @@
 const { sync } = require('./sync');
 const Repository = require('./repository');
+const { goDataSync } = require('./goDataSync');
 
 (async () => {
     try {
         await Repository.initRepository();   
-        console.log('Starting Dequeue item process....')     
-        await sync();
+        // console.log('Starting Dequeue item process....')     
+        //await sync();
+        await goDataSync();
     } catch (err) {
         console.log(err)
     }
