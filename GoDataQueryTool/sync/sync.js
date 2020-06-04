4a22bb0f72cd883a70e281e22e0c601414aa09aa
Ramiro Gonzalez <ramirogc21@gmail.com>
2020-06-04 20:59:07 -0300

    sync process update. log and configs added. call to issue service added

--- GoDataQueryTool/sync/sync.js
+++ GoDataQueryTool/sync/sync.js
@@ -1,22 +1,18 @@
+const IssueService = require('./services/issueService');
 const Queue = require('bull');
-const queueName = 'go-data-issues';
+const Config = require('config');
+const queueName = Config.get('queue_sync_go_data.name');
 const queue = new Queue(queueName);
 
 module.exports.start = async function () {
 
-
-
-    console.log(`Starting Dequeue item process from ${queueName}....`)     
+    console.log('sync',`Starting Dequeue item process from ${queueName}....`)     
 
     queue.process((job, done) => {
         try {        
             console.log(job.data);
             const issues = job.data;
-            issues.forEach(issue => {
-                 //call to service
-            });
-            
-           
+            await IssueService.syncReadDatabase(issues);
             done();
         } catch(err) {
             console.log(err);
