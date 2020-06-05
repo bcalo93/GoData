8825ec70a58667e9eaf37e0ccb7251f01c1e32c7
Ramiro Gonzalez <ramirogc21@gmail.com>
2020-06-04 21:01:37 -0300

    sync data access implemented

--- GoDataQueryTool/sync/dataAccess/issueDataAccess.js
+++ GoDataQueryTool/sync/dataAccess/issueDataAccess.js
@@ -1,2 +1,15 @@
-const Repository = require('./repository')
-const IssueRepository = Repository.Issue;
+const WriteRepository = require('../../repositories/writeRepository');
+
+module.exports = class WriteDataAccess {
+    constructor() {
+        this.issuesHistoryRepository = WriteRepository.Issue;
+    }
+
+    async save(issue) {
+        try {
+           return await this.issuesHistoryRepository.create(issue);
+        } catch (err) {
+            throw new Error('Database connection error')
+        }
+    }
+}
