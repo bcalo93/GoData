d30eea5554b8b53870be3cd9aa0631e39b11d3b8
Ramiro Gonzalez <ramirogc21@gmail.com>
2020-06-06 20:27:29 -0300

    index file updated. syncDb added

--- GoDataQueryTool/index.js
+++ GoDataQueryTool/index.js
@@ -1,6 +1,7 @@
 const Server = require('./server/server');
 const { QueryRepository, WriteRepository } = require('./repositories');
 const GoDataSync = require('./sync/sync');
+const GoDataSyncDb = require('./syncDb/syncDb');
 
 (async () => {
     try {
@@ -8,6 +9,7 @@ const GoDataSync = require('./sync/sync');
         await WriteRepository.initRepository();
         await Server.initServer();
         await GoDataSync.start();
+        await GoDataSyncDb.start();
     } catch (err) {
         console.log(err)
     }
