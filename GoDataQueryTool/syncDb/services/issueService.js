3b1f2051ae39e2ffb3239a4d92ceeb059ae3aa0b
Ramiro Gonzalez <ramirogc21@gmail.com>
2020-06-06 20:34:06 -0300

    using date utils functions. validation throw error

--- GoDataQueryTool/syncDb/services/issueService.js
+++ GoDataQueryTool/syncDb/services/issueService.js
@@ -1,4 +1,5 @@
 const IssueDataAccess = require('../dataAccess/issueDataAccess');
+const { parseToDate, isValidDate } = require('../../utils/dateUtils');
 
 module.exports = class IssueService {
     constructor() {
@@ -10,27 +11,34 @@ module.exports = class IssueService {
             for (let index = 0; index < issues.length; index++) {
                 const issue = issues[index];
                 if (this.validateIssue(issue)) {
-                    console.log('IssueService.insertIntoQueryRepository','Inserting issue ...');
-                    const insertedIssue = await this.issueDataAccess.save(issue);
-                    console.log('IssueService.insertIntoQueryRepository', `Issue id: ${insertedIssue.id} inserted ...`);
+                    console.log('IssueService.insertIntoQueryRepository','Inserting issue into documents...');
+                    const formattedIssue = this.formatIssue(issue);
+                    await this.issueDataAccess.persistIssueInDocuments(formattedIssue);
                 }
             }
         } catch (err) {
-            console.log('IssueService.insertIntoQueryRepository','Something went wrong ...')
+            console.log('IssueService.insertIntoQueryRepository','Something went wrong ...\n'+err)
         }
     }
 
     validateIssue(issue) {
         const isValidRegistrationState = issue.registrationState && issue.registrationState !== '';
         const isValidValidViolationCode = issue.violationCode && issue.violationCode !== '';
-        const isValidDate = issue.issueDate && this.parseToDate(issue.issueDate) instanceof Date;
-        return ( 
-            isValidRegistrationState
+        const issueDate = parseToDate(issue.issueDate);
+        const isValidIssueDate = issue.issueDate && isValidDate(issueDate);
+        if ( isValidRegistrationState
             && isValidValidViolationCode
-            && isValidDate );
+            && isValidIssueDate ) {
+                return true;
+        } else { 
+            const message = `Invalid Issue object. \nIssue: ${issue}\n isValidRegistrationState: ${isValidRegistrationState} - isValidValidViolationCode: ${isValidValidViolationCode} - isValidIssueDate: ${isValidIssueDate}`;
+            throw new Error(message);
+        }
     }
 
-    parseToDate(date) {
-        return new Date(date).toISOString();
+    formatIssue(issue) {
+        const year = issue.issueDate.getFullYear() || 0;
+        const month = issue.issueDate.getMonth() || 0;
+        return { ...issue, year, month };
     }
 }
