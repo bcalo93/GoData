c3458ed7174a63d0e74c69d7e8c2d71bd454ee3a
Ramiro Gonzalez <ramirogc21@gmail.com>
2020-06-06 20:33:31 -0300

    sync issue service fixed to catch individually issues errors. logs fixed date funcionts using utils

--- GoDataQueryTool/sync/services/issueService.js
+++ GoDataQueryTool/sync/services/issueService.js
@@ -1,11 +1,14 @@
 const IssueDataAccess = require('../dataAccess/issueDataAccess');
 const Queue = require('bull');
-const queueName = Config.get('queue_sync_db.name');
+const Config = require('config');
+const QUEUE_NAME = Config.get('queue_sync_db.name');
+const { parseToDate, isValidDate } = require('../../utils/dateUtils');
 
 module.exports = class IssueService {
     constructor() {
         this.issueDataAccess = new IssueDataAccess();
-        this.queue = new Queue(queueName);
+        console.log('sync.issueService', `Listening to queue: ${QUEUE_NAME}`);
+        this.queue = new Queue(QUEUE_NAME);
     }
 
     async syncReadDatabase(issues) {
@@ -13,12 +16,17 @@ module.exports = class IssueService {
         try {
             for (let index = 0; index < issues.length; index++) {
                 const issue = issues[index];
-                if (this.validateIssue(issue)) {
-                    console.log('IssueService.syncReadDatabase','Inserting issue ...');
-                    const insertedIssue = await this.issueDataAccess.save(issue);
-                    console.log('IssueService.syncReadDatabase', `Issue id: ${insertedIssue.id} inserted ...`);
-                    const formattedIssue = this.formatIssue(insertedIssue);
-                    issuesToSync.push(formattedIssue);
+                try {
+                    if (this.validateIssue(issue)) {
+                        console.log('IssueService.syncReadDatabase','Inserting issue ...');
+                        const insertedIssue = await this.issueDataAccess.save(issue);
+                        console.log('insertedIssue', insertedIssue);
+                        console.log('IssueService.syncReadDatabase', `Issue id: ${insertedIssue.id} inserted ...`);
+                        const formattedIssue = this.formatIssue(insertedIssue);
+                        issuesToSync.push(formattedIssue);
+                    }
+                } catch (err) {
+                    console.log('IssueService.syncReadDatabase',`Something went wrong when inserting ISSUE.SUMMONS_NUMBER ${issue.SUMMONS_NUMBER} ...\n`+err);
                 }
             }
             if (issuesToSync.length !== 0) {
@@ -26,24 +34,29 @@ module.exports = class IssueService {
                 this.queue.add(issuesToSync);
             }
         } catch (err) {
-            console.log('IssueService.syncReadDatabase','Something went wrong ...')
+            console.log('IssueService.syncReadDatabase','Something went wrong ...\n'+err);
         }
     }
 
     validateIssue(issue) {
         const isValidRegistrationState = issue.REGISTRATION_STATE && issue.REGISTRATION_STATE !== '';
         const isValidValidViolationCode = issue.VIOLATION_CODE && issue.VIOLATION_CODE !== '';
-        const isValidDate = issue.ISSUE_DATE && this.parseToDate(issue.ISSUE_DATE) instanceof Date;
-        return ( 
-            isValidRegistrationState
+        const issueDate = parseToDate(issue.ISSUE_DATE);
+        const isValidIssueDate = issue.ISSUE_DATE && isValidDate(issueDate);
+        if ( isValidRegistrationState
             && isValidValidViolationCode
-            && isValidDate );
+            && isValidIssueDate ) {
+                return true;
+        } else { 
+            const message = `Invalid Issue object. \nIssue: ${issue}\n isValidRegistrationState: ${isValidRegistrationState} - isValidValidViolationCode: ${isValidValidViolationCode} - isValidIssueDate: ${isValidIssueDate}`;
+            throw new Error(message);
+        }
     }
 
     formatIssue(issue) {
         const formattedIssue = (
             ({ REGISTRATION_STATE, VIOLATION_CODE, ISSUE_DATE }) => {
-                const issueDate = this.parseToDate(ISSUE_DATE);
+                const issueDate = parseToDate(ISSUE_DATE);
                 return { 
                     registrationState: REGISTRATION_STATE, 
                     violationCode: VIOLATION_CODE, 
@@ -53,9 +66,5 @@ module.exports = class IssueService {
         if (!!formattedIssue) {
             return formattedIssue;
         } else throw new Error("Format error");
-    }
-
-    parseToDate(date) {
-        return new Date(date).toISOString();
     }
 }
