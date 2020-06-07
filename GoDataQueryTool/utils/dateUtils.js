277309a72e103b46a0ed815d92e9b303d4e62e53
Ramiro Gonzalez <ramirogc21@gmail.com>
2020-06-06 21:16:21 -0300

    extracting common functions

--- GoDataQueryTool/utils/dateUtils.js
+++ GoDataQueryTool/utils/dateUtils.js
@@ -7,7 +7,17 @@ const isValidDate = (dateStr) => {
     return !isNaN(Date.parse(dateStr));
 }
 
+const getYearFromDate = (date) => {
+    return new Date(date).getFullYear() || 0;
+}
+
+const getMonthFromDate = (date) => {
+    return new Date(date).getMonth() || 0;
+}
+
 module.exports = {
     parseToDate, 
-    isValidDate
+    isValidDate,
+    getYearFromDate,
+    getMonthFromDate
 }
