3a08de81dbb7aa309c88a537314aa3fb2940eb5f
Ramiro Gonzalez <ramirogc21@gmail.com>
2020-06-19 01:59:21 -0300

    data access refactor. functions splitted into modules

--- GoDataQueryTool/syncDb/dataAccess/issueQueryDataAccess.js
+++ GoDataQueryTool/syncDb/dataAccess/issueQueryDataAccess.js
@@ -1,5 +1,7 @@
 const QueryRepository = require('../../repositories/queryRepository');
- module.exports = class IssueQueryDataAccess {
+const { persistIssueYear, persistIssueCodes, persistIssueStates } = require('../dataAccess');
+
+module.exports = class IssueQueryDataAccess {
      constructor() {
          this.issueYearRepository = QueryRepository.IssuesYear;
          this.issueCodesRepository = QueryRepository.IssuesCodes;
@@ -8,61 +10,13 @@ const QueryRepository = require('../../repositories/queryRepository');
 
     async persistIssueInDocuments(issue) {
         try{
-            await this.persistIssueYear(issue);
-            await this.persistIssueCodes(issue);
-            await this.persistIssueStates(issue);
+            console.log('IssueQueryDataAccess.persistIssueInDocuments', `Issue to persist: ${JSON.stringify(issue)}`);
+            await persistIssueYear(issue, this.issueYearRepository);
+            await persistIssueCodes(issue, this.issueCodesRepository);
+            await persistIssueStates(issue, this.issueStatesRepository);
         } catch (err) {
             throw new Error(err)
         }
     }
 
-    async persistIssueYear(issue) {
-        if (issue.year && issue.year !== 0 && issue.month && issue.month !== 0) {
-            const document = {
-                year: issue.year,
-                "issues.month": issue.month
-            }
-        
-            const update = { 
-                $inc: { "months.$.count": 1 } 
-            }
-            await this.issueYearRepository.updateOne(document, update, (err, raw) => {
-                if(err) {
-                    console.log('issueDataAccess.persistIssueYear', err)
-                }
-            }).exec();
-        }
-    }
-    
-    async persistIssueCodes(issue) {
-        const document = {
-            violationCode: issue.violationCode,
-            "issues.date": issue.issueDate
-        }
-    
-        const update = { 
-            $inc: { "issues.$.count": 1 } 
-        }
-        await this.issueCodesRepository.updateOne(document, update, (err, raw) => {
-            if(err) {
-                console.log('issueDataAccess.persistIssueCodes', err)
-            }
-        }).exec();
-    }
-
-    async persistIssueStates(issue) {
-        const document = {
-            registrationState: issue.registrationState,
-            "issues.date": issue.issueDate
-        }
-    
-        const update = { 
-            $inc: { "issues.$.count": 1 } 
-        }
-        await this.issueStatesRepository.updateOne(document, update, (err, raw) => {
-            if(err) {
-                console.log('issueDataAccess.persistIssueStates', err)
-            }
-        }).exec();
-    }
  }
