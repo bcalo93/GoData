3f281bd7e20e8ef2ede6980dab47f718ae4ee356
Martín <martincv@gmail.com>
2020-06-27 00:29:22 -0300

    StateDataService completed

--- StateInfo/data-service.js
+++ StateInfo/data-service.js
@@ -3,11 +3,9 @@ const redis = require('redis')
 const HttpClient = require('./http-client')
 const util = require("util")
 const { EventEmitter } = require('events')
-
 const axiosConfig = config.get('axios_config')
 
-const PLATE_CONFIG = config.get('data_config.plates')
-const STATE_CONFIG = config.get('data_config.states')
+const dataConfig = config.get('data_config')
 
 module.exports = class DataService {
 
@@ -21,15 +19,16 @@ module.exports = class DataService {
         this.cacheGetAsync = util.promisify(this.redisClient.hget).bind(this.redisClient)
         this.cacheExistsAsync = util.promisify(this.redisClient.hexists).bind(this.redisClient)
         this.cacheSetAsync = util.promisify(this.redisClient.hset).bind(this.redisClient)
+        this.cacheGetAllAsync = util.promisify(this.redisClient.hgetall).bind(this.redisClient)
         return emitter
     }
 
     static getType = (plate) => {
-        return this.get(PLATE_CONFIG, plate)
+        return this.get(dataConfig.plates, plate)
     }
     
     static getState = (code) => {
-        return this.get(STATE_CONFIG, code)
+        return this.get(dataConfig.states, code)
     }
 
     static get = async(config, id) => {
@@ -63,5 +62,35 @@ module.exports = class DataService {
                 since:Date.now()
             }))
         return data
+    }
+
+    static initDataUpdater = async () => {
+        const interval = dataConfig.update_interval * 1000 * 60
+        this.interval = setInterval(async () => {
+            await this.update(dataConfig.plates)
+            await this.update(dataConfig.states)
+        }, interval)
+    }
+
+    static stopDataUpdater = () => {
+        clearInterval(this.interval);
+    }
+
+    static update = async (config) => {
+        let data = await this.cacheGetAllAsync(config.key)
+        for(const key in data){
+            const info = JSON.parse(data[key])
+            const staleTime = dataConfig.stale_since_m * 1000 * 60
+            
+            if((Date.now() - info.since) > staleTime){
+                console.log(`[${key}] Old data detected. Updating`)
+                try {
+                    await this.getAndSetFromApi(config,key)
+                } catch(err) {
+                    console.log(err)
+                }
+            }
+            console.log(info)
+        }
     }
 }
--- StateInfo/test.js
+++ StateInfo/test.js
@@ -7,11 +7,17 @@
 
     try {
         await console.log(await DataService.getType("AGR"))
+        await console.log(await DataService.getType("DLR"))
+        await console.log(await DataService.getType("FAR"))
+
         await console.log(await DataService.getState("YT"))
+        await console.log(await DataService.getState("NY"))
+        await console.log(await DataService.getState("NJ"))
+        await DataService.initDataUpdater()
+
+        setTimeout(() => DataService.stopDataUpdater(),1000*60*4)
     } catch(err) {
         console.error('Error')
     }
-
-    setTimeout(async () => {await console.log(await DataService.getState("YT"))},10000)
 })
 ()
