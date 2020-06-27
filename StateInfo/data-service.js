c81d907b8f1efacd4fcc5d7145188bf44978dbae
Martín <martincv@gmail.com>
2020-06-26 23:16:03 -0300

    Refactor

--- StateInfo/data-service.js
+++ StateInfo/data-service.js
@@ -1,65 +1,67 @@
 const config = require('config')
 const redis = require('redis')
-const client = redis.createClient()
 const HttpClient = require('./http-client')
-const axiosConfig = config.get('axios_config')
-const httpClient = new HttpClient(axiosConfig)
 const util = require("util")
+const { EventEmitter } = require('events')
 
-
-client.on("error", function(error) {
-    console.error(error);
-});
-
-const getAsync = util.promisify(client.hget).bind(client)
-const existsAsync = util.promisify(client.hexists).bind(client)
-const setAsync = util.promisify(client.hset).bind(client)
+const axiosConfig = config.get('axios_config')
 
 const PLATE_CONFIG = config.get('data_config.plates')
 const STATE_CONFIG = config.get('data_config.states')
 
-const getType = (plate) => {
-    return get(PLATE_CONFIG, plate)
-}
+module.exports = class DataService {
 
-const getState = (code) => {
-    return get(STATE_CONFIG, code)
-}
+    static initialize() {
+        const emitter = new EventEmitter()
+        this.httpClient = new HttpClient(axiosConfig)
+        this.redisClient = redis.createClient()
+        this.redisClient.on("error", function(error) {
+            emitter.emit('redis_error',error)
+        });
+        this.cacheGetAsync = util.promisify(this.redisClient.hget).bind(this.redisClient)
+        this.cacheExistsAsync = util.promisify(this.redisClient.hexists).bind(this.redisClient)
+        this.cacheSetAsync = util.promisify(this.redisClient.hset).bind(this.redisClient)
+        return emitter
+    }
 
-const get = async(config, id) => {
-    return new Promise(async (resolve, reject) => {
-        try {
-            if(await existsAsync(config.key,id)){
-                let result = JSON.parse(await getAsync(config.key,id))
-                resolve(result.value)
-            } else {
-                try{
-                    console.log(`consultando API ${config.uri} ${id}`)
-                    let data = getAndSetFromApi(config, id)
-                    resolve(data)
-                } catch(error) {
-                    reject('Error getting info from StateData')
+    static getType = (plate) => {
+        return this.get(PLATE_CONFIG, plate)
+    }
+    
+    static getState = (code) => {
+        return this.get(STATE_CONFIG, code)
+    }
+
+    static get = async(config, id) => {
+        return new Promise(async (resolve, reject) => {
+            try {
+                if(await this.cacheExistsAsync(config.key,id)){
+                    let result = JSON.parse(await this.cacheGetAsync(config.key,id))
+                    resolve(result.value)
+                } else {
+                    try{
+                        console.log(`consultando API ${config.uri} ${id}`)
+                        let data = this.getAndSetFromApi(config, id)
+                        resolve(data)
+                    } catch(error) {
+                        reject('Error getting info from StateData')
+                    }
                 }
+            } catch(error) {
+                reject(error)
             }
-        } catch(error) {
-            reject(error)
-        }
-    })
-}
+        })
+    }
 
-const getAndSetFromApi = async(config, id) => {
-    let result = await httpClient.getWithParam(config.uri, id)
-    let data = result.data[config.field]
-    await setAsync(config.key,id,
-        JSON.stringify(
-        {
-            value:data,
-            since:Date.now()
-        }))
-    return data
-}
-
-module.exports = {
-    getType,
-    getState
+    static getAndSetFromApi = async(config, id) => {
+        let result = await this.httpClient.getWithParam(config.uri, id)
+        let data = result.data[config.field]
+        await this.cacheSetAsync(config.key,id,
+            JSON.stringify(
+            {
+                value:data,
+                since:Date.now()
+            }))
+        return data
+    }
 }
--- StateInfo/index.js
+++ StateInfo/index.js
@@ -1,12 +1,17 @@
 
 (async() => {
-    const service = require('./data-service')
+    const DataService = require('./data-service')
+    DataService.initialize().on('redis_error', (error) => {
+        console.error(`Error de redis. ${error.message}`)
+    })
 
     try {
-        await console.log(await service.getType("AGR"))
-        await console.log(await service.getState("YT"))
+        await console.log(await DataService.getType("AGR"))
+        await console.log(await DataService.getState("YT"))
     } catch(err) {
-        console.error(err)
+        console.error('Error')
     }
+
+    setTimeout(async () => {await console.log(await DataService.getState("YT"))},10000)
 })
 ()
